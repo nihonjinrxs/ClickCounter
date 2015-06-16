942711c5532fa779a8c7031afde7576371349f9c
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-16 01:10:18 -0400

    Added features: decrement button, extra label, color change button

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var count = 0
     var label:UILabel!
+    var label2:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,18 +23,59 @@ class ViewController: UIViewController {
         self.view.addSubview(label)
         self.label = label
         
-        // Button
-        let button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        // Label2
+        let label2 = UILabel()
+        label2.frame = CGRectMake(250, 150, 60, 60)
+        label2.text = "0"
+        self.view.addSubview(label2)
+        self.label2 = label2
+        
+        // Increment Button
+        let upButton = UIButton()
+        upButton.frame = CGRectMake(150, 250, 60, 60)
+        upButton.setTitle("Up", forState: .Normal)
+        upButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(upButton)
+        upButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+
+        // Decrement Button
+        let downButton = UIButton()
+        downButton.frame = CGRectMake(250, 250, 60, 60)
+        downButton.setTitle("Down", forState: .Normal)
+        downButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(downButton)
+        downButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Color Change Button
+        let colorButton = UIButton()
+        colorButton.frame = CGRectMake(200, 350, 180, 60)
+        colorButton.setTitle("Change Color", forState: .Normal)
+        colorButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(colorButton)
+        colorButton.addTarget(self, action: "changeBackground", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
     func incrementCount() {
         self.count++
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+    
+    func decrementCount() {
+        self.count--
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+    
+    func getRandomColor() -> UIColor {
+        let randomRed:CGFloat = CGFloat(drand48())
+        let randomGreen:CGFloat = CGFloat(drand48())
+        let randomBlue:CGFloat = CGFloat(drand48())
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    func changeBackground() {
+        self.view.backgroundColor = getRandomColor()
     }
 }
 
